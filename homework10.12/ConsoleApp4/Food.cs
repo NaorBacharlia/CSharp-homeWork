756ed82583d4d6991f75421d24e92f2ee8ffa0b1
Naor bacharlia <44392584+NaorBacharlia@users.noreply.github.com>
2018-12-16 18:20:57 +0200

    homework 10.12

---
+++ homework10.12/ConsoleApp4/Food.cs
@@ -0,0 +1,77 @@
+﻿using System;
+namespace ConsoleApp4
+{
+	class Food
+	{
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private int price;
+
+		public int Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+		private bool kosher;
+
+		public bool Kosher
+		{
+			get { return kosher; }
+			set { kosher = value; }
+		}
+
+		private string manufacturer_name;
+
+		public string Manufacturer_name
+		{
+			get { return manufacturer_name; }
+			set { manufacturer_name = value; }
+		}
+
+		private int year;
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+		//--------------------ctor-------------------------
+		public Food(string name,int price,bool kosher,string manufacturer_name,int year)
+		{
+			Name = name;
+			Price = price;
+			Kosher = kosher;
+			Manufacturer_name = manufacturer_name;
+			Year = year;
+		}
+		//--------------------method to print the properties-------------------------
+		public void print_prudect()
+		{
+			string k;
+			if (kosher == true)
+				k = "kosher";
+			else
+				k = "Not kosher";
+			Console.WriteLine($"Name:{name}\n" +
+				              $"Price:{price}\n" +
+				              $"{k}\n" +
+				              $"Manufacturer name:{manufacturer_name}\n" +
+				              $"Expiration year:{year}\n");
+		}
+		//--------------------method to check if the is expiry date-------------------------
+		public bool Expiry_Date(int Cuerrent_Year)
+		{
+			if (year < Cuerrent_Year)
+				return false;
+			else
+				return true;
+		}
+	}
+}
---
+++ homework10.12/ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string currentYear = DateTime.Now.Year.ToString();
+			int Year=Convert.ToInt32(currentYear);
+			Food[] list = new Food[5];
+			list[0] = new Food("Bamba",5,true,"osem",2017);
+			list[1] = new Food("Ice cream",10, false, "nestle", 2018);
+			list[2] = new Food("Hot Dog",35, false, "yanay", 2019);
+			list[3] = new Food("Hamborger",55, true, "yager", 2017);
+			list[4] = new Food("Pocporen", 47, true, "cinima",2020);
+			for (int i = 0; i < list.Length; i++)
+			{
+				Console.WriteLine($"---------------------------------Product No.{i+1}-----------------------------------");
+				list[i].print_prudect();
+				if (list[i].Expiry_Date(Year))
+					Console.WriteLine("The date is good.");
+				else
+					Console.WriteLine("***The product date is not good.***");
+			}
+		}
+	}
+}
